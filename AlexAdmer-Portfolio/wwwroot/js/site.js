b050fd375d8f29c7635cfa841dec0336caded9fb
AlexAdmer03 <alex.admer03@gmail.com>
2023-05-26 17:33:47 +0200

    Done some styling

--- AlexAdmer-Portfolio/wwwroot/js/site.js
+++ AlexAdmer-Portfolio/wwwroot/js/site.js
@@ -2,3 +2,15 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+//$(window).scroll(function () {
+//    var scroll = $(window).scrollTop();
+
+//    if (scroll >= $('#about').position().top) { // Check if page is scrolled to #about section.
+//        $(".navbar").removeClass("navbar-top");
+//        $(".navbar").addClass("navbar-scroll");
+//    } else {
+//        $(".navbar").removeClass("navbar-scroll");
+//        $(".navbar").addClass("navbar-top");
+//    }
+//});
